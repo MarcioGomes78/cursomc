97cba51a80daec5d69cc5467477388fbeb0391d2
MarcioGomes78 <mjgomes19782gmail.com>
2022-06-27 23:37:06 -0300

    Endpoint /clientes/{id} disponivel

--- src/main/java/com/marciogomes/cursomc/domain/Endereco.java
+++ src/main/java/com/marciogomes/cursomc/domain/Endereco.java
@@ -9,6 +9,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
 public class Endereco implements Serializable{
 	private static final long serialVersionUID = 1L;
@@ -22,6 +24,7 @@ public class Endereco implements Serializable{
 	private String bairro;
 	private String cep;
 	
+	@JsonBackReference
 	@ManyToOne
 	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
@@ -94,11 +97,11 @@ public class Endereco implements Serializable{
 		this.cep = cep;
 	}
 
-	public Cliente getCliebte() {
+	public Cliente getCliente() {
 		return cliente;
 	}
 
-	public void setCliebte(Cliente cliente) {
+	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
 	}
 
---
+++ src/main/java/com/marciogomes/cursomc/services/ClienteService.java
@@ -0,0 +1,26 @@
+package com.marciogomes.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marciogomes.cursomc.domain.Cliente;
+import com.marciogomes.cursomc.repositories.ClienteRepository;
+import com.marciogomes.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName());
+		} 
+		return obj.orElse(null);
+	}
+	
+}
