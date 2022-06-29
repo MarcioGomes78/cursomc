ac739c458cd37f5b7dde40d3b9c85d9d825db4a0
MarcioGomes78 <mjgomes19782gmail.com>
2022-06-28 23:00:49 -0300

    Endpoint /pedidos/{id} disponibilizados

--- src/main/java/com/marciogomes/cursomc/domain/ItemPedido.java
+++ src/main/java/com/marciogomes/cursomc/domain/ItemPedido.java
@@ -5,10 +5,13 @@ import java.io.Serializable;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 public class ItemPedido implements Serializable{
 	private static final long serialVersionUID = 1L;
-
+	
+	@JsonIgnore
 	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
 	
@@ -31,9 +34,11 @@ public class ItemPedido implements Serializable{
 
 	//Para ter acesso direto ao Pedido e o Produto fora da minha class ItemPedido
 	//Isso melhora asemantica da minha class
+	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
+	
 	public Produto getProduto() {
 		return id.getProduto();
 	}
---
+++ src/main/java/com/marciogomes/cursomc/services/PedidoService.java
@@ -0,0 +1,26 @@
+package com.marciogomes.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marciogomes.cursomc.domain.Pedido;
+import com.marciogomes.cursomc.repositories.PedidoRepository;
+import com.marciogomes.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName());
+		} 
+		return obj.orElse(null);
+	}
+	
+}
