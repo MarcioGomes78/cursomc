280650937f369bc34bbfec9bc38bbe7ecf0ddb20
MarcioGomes78 <mjgomes1978@gmail.com>
2022-07-19 19:38:32 -0300

    Subtotal e total do pedido

--- src/main/java/com/marciogomes/cursomc/domain/ItemPedido.java
+++ src/main/java/com/marciogomes/cursomc/domain/ItemPedido.java
@@ -32,6 +32,10 @@ public class ItemPedido implements Serializable{
 		this.preco = preco;
 	}
 
+	public double getSubTotal() {
+		return (preco - desconto) * quantidade;
+	}
+	
 	//Para ter acesso direto ao Pedido e o Produto fora da minha class ItemPedido
 	//Isso melhora asemantica da minha class
 	@JsonIgnore
